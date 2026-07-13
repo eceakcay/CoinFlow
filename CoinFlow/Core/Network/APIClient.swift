814fe5ab4e3126a5cee8c08ab609ecb71d5c6d46
eceakcay <eceakcay.36@gmail.com>
2026-07-13 14:19:28 +0300

    Add APIClient and Endpoint

--- CoinFlow/Core/Network/APIClient.swift
+++ CoinFlow/Core/Network/APIClient.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+
+final class APIClient {
+    
+    func request<T: Decodable>(_ endpoint: Endpoint) async throws -> T {
+    
+        guard var components = URLComponents(string: endpoint.baseURL + endpoint.path) else {
+            throw NetworkError.invalidURL
+        }
+        
+        components.queryItems = endpoint.queryItems
+        
+        guard let url = components.url else {
+            throw NetworkError.invalidURL
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = endpoint.method.rawValue
+        request.httpBody = endpoint.body
+        
+        endpoint.headers?.forEach { key, value in
+            request.setValue(value, forHTTPHeaderField: key)
+        }
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                throw NetworkError.invalidResponse
+            }
+            
+            guard 200...299 ~= httpResponse.statusCode else {
+                throw NetworkError.statusCode(httpResponse.statusCode)
+            }
+            
+            do {
+                return try JSONDecoder().decode(T.self, from: data)
+            } catch {
+                throw NetworkError.decodingError
+            }
+        } catch let error as NetworkError {
+            throw error
+        }
+        catch {
+            throw NetworkError.unknown(error)
+        }
+        
+    }
+}
+
--- CoinFlow/Core/Network/Endpoint.swift
+++ CoinFlow/Core/Network/Endpoint.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+protocol Endpoint {
+    var baseURL: String { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var queryItems: [URLQueryItem]? { get }
+    var headers: [String: String]? { get }
+    var body: Data? { get }
+}
