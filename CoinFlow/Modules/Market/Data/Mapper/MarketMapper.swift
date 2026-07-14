7178a6b9f937dbb99f8e9edf9027cc527b110873
eceakcay <eceakcay.36@gmail.com>
2026-07-14 10:42:00 +0300

    Implement CryptoCurrency DTO, entity and mapper

---
+++ CoinFlow/Modules/Market/Data/Mapper/MarketMapper.swift
@@ -0,0 +1,8 @@
+//
+//  MarketMapper.swift
+//  CoinFlow
+//
+//  Created by Ece Akcay on 14.07.2026.
+//
+
+import Foundation
---
+++ CoinFlow/Modules/Market/Domain/Entity/CryptoCurrency.swift
@@ -0,0 +1,8 @@
+//
+//  CryptoCurrency.swift
+//  CoinFlow
+//
+//  Created by Ece Akcay on 13.07.2026.
+//
+
+import Foundation
