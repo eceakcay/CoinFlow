fb58f858b45ed86dcf6046aabbf9575ac610980e
eceakcay <eceakcay.36@gmail.com>
2026-08-20 14:12:17 +0300

    Add API key example configuration

---
+++ CoinFlow/Core/Config/APIKeys.example.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.example.swift
+//  CoinFlow
+//
+//  Created by Ece Akcay on 20.08.2026.
+//
+
+import Foundation
+
+enum APIKeysExample {
+
+    static let coinGeckoAPIKey = "YOUR_API_KEY_HERE"
+}
